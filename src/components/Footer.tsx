227bb029098534f705037b08efbc1d913baf640c
Glenn Torrens <glenntorrens@gmail.com>
2025-11-11 18:14:05 +0900

    implemented footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { footerLinks } from '../constants'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className='info'>
+        <p>Find an Apple Store near you, or call 1-800-MY-APPLE.</p>
+        <img src="/logo.svg" alt="apple logo" />
+
+        <hr />
+
+        <div className='links'>
+          <p>Copyright © 2025 Apple Inc. All rights reserved.</p>
+          <ul>
+            {footerLinks.map(({label, link}) => (
+              <li key={label}>
+                <a href={link}>{label}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </footer>
   )
 }
 
